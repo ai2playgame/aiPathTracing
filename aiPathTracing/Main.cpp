239f10e94ed6f70221081ea3da874ae81a1b3a3d
ai2playgame <detective9684@gmail.com>
2020-08-30 20:52:38 +0900

    Update:

--- aiPathTracing/Main.cpp
+++ aiPathTracing/Main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-#include <edupt/ray.h>
 
-int main() {
-	std::cout << "Hello, path tracing!" << std::endl;
-	std::cout << "ハロー、パストレーシング！" << std::endl;
+#pragma warning(disable:4996)
+#include <edupt/render.h>
+
+int main(int argc, char **argv) {
+	std::cout << "Path tracing renderer: edupt" << std::endl << std::endl;
+
+	// 640x480の画像、(2x2) * 4 sample / pixel
+	edupt::render(640, 480, 128, 2);
 }
